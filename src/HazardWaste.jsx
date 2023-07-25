d38307b35ffe76a5a2daa1504473ca50327c6adc
Olive <njerio86@gmail.com>
2023-07-25 21:35:19 +0300

    updated the products section

--- src/HazardWaste.jsx
+++ src/HazardWaste.jsx
@@ -6,6 +6,77 @@ function Hazard() {
       <div class="hero hero-1 d-flex">
         <h1>HAZARDOUS WASTE MANAGEMENT</h1>
       </div>
+      <div className="container">
+        <h4>Waste Management</h4>
+        <p>
+          Waste and by-products cover a diverse range of materials, as the
+          following list illustrates:
+          <ol>
+            <li>
+              <strong>Infectious waste:</strong>
+            </li>
+            <p>
+              waste contaminated with blood and other bodily fluids (e.g. from
+              discarded diagnostic samples), cultures and stocks of infectious
+              agents from laboratory work (e.g. waste from autopsies and
+              infected animals from laboratories), or waste from patients with
+              infections (e.g. swabs, bandages and disposable medical devices);
+            </p>
+            <li>
+              <strong>Pathological waste:</strong>
+            </li>
+            <p>
+              human tissues, organs or fluids, body parts and contaminated
+              animal carcasses;
+            </p>
+            <li>
+              <strong>Sharps waste:</strong>
+            </li>
+            <p>syringes, needles, disposable scalpels and blades, etc.;</p>
+            <li>
+              <strong>Chemical waste:</strong>
+            </li>
+            <p>
+              for example solvents and reagents used for laboratory
+              preparations, disinfectants, sterilants and heavy metals contained
+              in medical devices (e.g. mercury in broken thermometers) and
+              batteries;
+            </p>
+            <li>
+              <strong>Pharmaceutical waste:</strong>
+            </li>
+            <p>expired, unused and contaminated drugs and vaccines;</p>
+            <li>
+              <strong>Cytotoxic waste:</strong>
+            </li>
+            <p>
+              waste containing substances with genotoxic properties (i.e. highly
+              hazardous substances that are, mutagenic, teratogenic or
+              carcinogenic), such as cytotoxic drugs used in cancer treatment
+              and their metabolites;
+            </p>
+            <li>
+              <strong>Radioactive waste:</strong>
+            </li>
+            <p>
+              such as products contaminated by radionuclides including
+              radioactive diagnostic material or radiotherapeutic materials;
+            </p>
+            <li>
+              <strong>Non-hazardous or general waste:</strong>
+            </li>
+            <p>
+              waste that does not pose any particular biological, chemical,
+              radioactive or physical hazard.
+            </p>
+            <br />
+            <p>
+              All our waste are normally disposed and burnt in our hospital
+              incinerators.
+            </p>
+          </ol>
+        </p>
+      </div>
     </div>
   );
 }
--- src/Rescue.jsx
+++ src/Rescue.jsx
@@ -1,10 +1,65 @@
 import React from "react";
+import "./rescue.css";
 
 function Rescue() {
   return (
     <div>
       <div class="hero hero-1 d-flex">
         <h1>RESCUE</h1>
+      </div>
+      <div className="container">
+        <h4 className="mt-3">Accident & Emergency</h4>
+        <p>
+          A&E patients present with potentially life threatening symptoms such
+          as headache, chest pain, abdominal pain, collapse of unknown cause and
+          severe injury. Such patients have pressing need(s) and may present
+          without prior appointment. They may also report to the unit on their
+          own or by ambulance. It uses a triage system of screening and
+          classifying clients to determine their priority needs and to measure
+          patient care efficiently and is usually done by a qualified nurse.
+          Some of the groupings include:
+          <br />
+          <ul className="centered-ul text-start">
+            <li>Peadiatric patients</li>
+            <li>Pregnant mothers</li>
+            <li>The elderly</li>
+            <li>School going kids</li>
+            <li>Critical intervention of all kind</li>
+          </ul>
+          <br />
+          Once the nurse at triage assess all patients presenting to the A&E
+          she/he transfers them to a medical officers in the unit who then
+          involves expanded team which include: Paediatrician, Gynaecologist,
+          and COSECSA Registrars for critical decision making. The medical
+          officer on duty may request consultation with another specialist for a
+          patient in the A & E."Referral to Consultants Clinic or Hospital of
+          Choice"
+          <br />
+          Procedures offered in the Accident and Emergency Unit:
+          <ul className="text-start">
+            <li>ECG</li>
+            <li>Ear Syringing</li>
+            <li>Drug Administration</li>
+            <li>Pap Smears</li>
+            <li>Nebulizations</li>
+            <li>Wound Care/Dressing</li>
+            <li>Suturing of Wounds</li>
+            <li>Minor procedures i.e. incision of drainage</li>
+            <li>Plaster Applications and Removal</li>
+            <li>Fluid Therapy Replacement</li>
+          </ul>
+          <br />
+          In case of Emergencies our Emergency Rooms are well equipped with
+          trained personnel and all the required items i.e. Monitors and Vents.
+          Ambulance well equipped with well trained personnel. Other services
+          offered in the have unit include:
+          <ul className="text-start">
+            <li>Sexual Gender Based Violence (SGBV)</li>
+            <li>
+              Laboratory and Radiology services(to fast track patients care)
+            </li>
+          </ul>
+        </p>
       </div>
     </div>
   );
