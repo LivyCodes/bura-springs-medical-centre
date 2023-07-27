dd15a299a17bf4b117114c7e0ff82ad5dd98d15b
Olive <njerio86@gmail.com>
2023-07-27 15:29:08 +0300

    updated logo.jsx

--- src/logo.jsx
+++ src/logo.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import "./logo.css";
 import logo from "./assets/logo.jpg";
-import lindamama from "./assets/lindamama.jpg";
-import eduafya from "./assets/eduafya.jpg";
-import nhif from "./assets/Nhif.jpg";
+import lindamama from "./assets/lindamama.png";
+import eduafya from "./assets/eduafya.png";
+import nhif from "./assets/Nhif.png";
 
 function Logo() {
   return (
-    <div className="heros">
-      <img src={logo} alt="logo" height={120} width={120} />
-      <img src={lindamama} alt="logo" height={120} width={120} />
-      <img src={nhif} alt="logo" height={120} width={120} />
-      <img src={eduafya} alt="logo" height={120} width={120} />
-      <div className="justify-content-center text-center">
+    <div className="heros ">
+      <img src={logo} alt="logo" height={270.742} width={270.742} />
+      <div className="my-3 text-center d-flex flex-column align-items-center">
         <h1>BURA SPRINGS FAMILY MEDICAL CENTRE LTD</h1>
         <h4 className="best-of">
           <span>Best of Care,</span>
           <span>Close to Home</span>
         </h4>
+        <div className="d-flex justify-content-end logos mt-3">
+          {" "}
+          <img src={lindamama} alt="logo" height={130} width={130} />
+          <img src={nhif} alt="logo" height={130} width={130} />
+          <img src={eduafya} alt="logo" height={130} width={130} />
+        </div>
       </div>
     </div>
   );
