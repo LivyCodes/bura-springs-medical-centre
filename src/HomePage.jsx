a61df675b72569b9ca7d4b586b9bd37e3d9d2af7
Olive <njerio86@gmail.com>
2023-07-28 12:43:31 +0300

    updated the image carousel

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,6 +6,16 @@ import staff from "./assets/staff.jpeg";
 import heartrate from "./assets/heartrate.jpg";
 import stethoscope from "./assets/stethoscope.jpg";
 import doctor from "./assets/doctor.jpg";
+import reception from "./assets/reception.jpeg";
+import hospital from "./assets/hos.jpeg";
+import washrooms from "./assets/washrooms.jpeg";
+import meds from "./assets/meds.jpeg";
+import tanks from "./assets/tanks.jpeg";
+import ward from "./assets/ward.jpeg";
+import physiotherapy from "./assets/physiotherapy.jpeg";
+import pharmacy from "./assets/pharmacy.jpeg";
+import maternity from "./assets/maternity-bed2.jpeg";
+import triage from "./assets/triage.jpeg";
 
 function HomePage() {
   return (
@@ -24,16 +34,36 @@ function HomePage() {
                   <p>Bura Springs Medical Centre Ltd</p>
                 </div>
                 <div class="carousel-item">
+                  <img src={hospital} className="hospital-image" />
+                </div>
+                <div class="carousel-item">
+                  <img src={reception} className="hospital-image" />
+                  <p>Facility Reception</p>
+                </div>
+
+                <div class="carousel-item">
+                  <img src={physiotherapy} className="hospital-image" />
+                  <p>Physiotherapy Room</p>
+                </div>
+                <div class="carousel-item">
+                  <img src={maternity} className="hospital-image" />
+                  <p>Maternity Room</p>
+                </div>
+                <div class="carousel-item">
+                  <img src={pharmacy} className="hospital-image" />
+                  <p>Pharmacy</p>
+                </div>
+                <div class="carousel-item">
+                  <img src={triage} className="hospital-image" />
+                  <p>Triage</p>
+                </div>
+                <div class="carousel-item">
+                  <img src={ward} className="hospital-image" />
+                  <p>Ward</p>
+                </div>
+                <div class="carousel-item">
                   <img src={heartrate} className="hospital-image" />
                   <p>Patient in consultation</p>
-                </div>
-                <div class="carousel-item">
-                  <img src={stethoscope} className="hospital-image" />
-                  <p>A Stethoscope</p>
-                </div>
-                <div class="carousel-item">
-                  <img src={doctor} className="hospital-image" />
-                  <p>An attending</p>
                 </div>
                 <div class="carousel-item">
                   <img src={staff} className="hospital-image" />
@@ -42,6 +72,17 @@ function HomePage() {
                 <div class="carousel-item">
                   <img src={certs} className="hospital-image" />
                   <p>Facility operation approval certificates</p>
+                </div>
+                <div class="carousel-item">
+                  <img src={meds} className="hospital-image" />
+                </div>
+                <div class="carousel-item">
+                  <img src={tanks} className="hospital-image" />
+                  <p>Water Tanks</p>
+                </div>
+                <div class="carousel-item">
+                  <img src={washrooms} className="hospital-image" />
+                  <p>Washrooms</p>
                 </div>
               </div>
               <button
